f97a5dce078337a744255d1b4643825f89c8b13f
berloc <berloc1987@gmail.com>
2017-09-29 11:31:09 +0200

    initial commit

---
+++ src/main/java/com/vodafone/mock/mockg4s/Mockg4sApplication.java
@@ -0,0 +1,12 @@
+package com.vodafone.mock.mockg4s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mockg4sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mockg4sApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vodafone/mock/mockg4s/Mockg4sApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vodafone.mock.mockg4s;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mockg4sApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
