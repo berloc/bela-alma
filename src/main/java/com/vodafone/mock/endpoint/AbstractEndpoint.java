2d8bb3537f67c38b558ec1ef59814956e1d7e457
berloc <berloc1987@gmail.com>
2017-10-03 09:50:11 +0200

    add AbstractEndpoint

--- src/main/java/com/vodafone/mock/endpoint/AbstractEndpoint.java
+++ src/main/java/com/vodafone/mock/endpoint/AbstractEndpoint.java
@@ -1,4 +1,9 @@
 package com.vodafone.mock.endpoint;
 
-public class AbstractEndpoint {
+import com.vodafone.mock.model.ObjectFactory;
+
+public abstract class AbstractEndpoint {
+
+    static final String NAMESPACE_URI = "http://netlife.hu/g4futar/webservice/";
+    ObjectFactory factory = new ObjectFactory();
 }
