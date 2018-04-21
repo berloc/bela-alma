0ba7eff616bd2a4dd757e8b22f1ea19e1d210979
berloc <berloc1987@gmail.com>
2018-04-21 11:07:07 +0200

    build war file instead of jar

--- src/main/java/com/vodafone/mock/Mockg4sApplication.java
+++ src/main/java/com/vodafone/mock/Mockg4sApplication.java
@@ -2,9 +2,16 @@ package com.vodafone.mock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Mockg4sApplication {
+public class Mockg4sApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Mockg4sApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Mockg4sApplication.class, args);
